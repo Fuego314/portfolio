55f96e2a614785d3dccfb967090cb3f333769344
Fuego314 <l.tucker@hotmail.co.uk>
2017-02-26 03:22:15 +0000

    Swap variable

--- work/jmb/js/main.js
+++ work/jmb/js/main.js
@@ -3,7 +3,7 @@
 
 		// SCROLL TO TOP
 		// browser window scroll (in pixels) after which the "back to top" link is shown
-		let offset = 300,
+		let scrollOffset = 300,
 			//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
 			offset_opacity = 1200,
 			//duration of the top scrolling animation (in ms)
@@ -13,7 +13,7 @@
 
 		//hide or show the "back to top" link
 		$(window).scroll(function(){
-			( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+			( $(this).scrollTop() > scrollOffset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
 			if( $(this).scrollTop() > offset_opacity ) {
 				$back_to_top.addClass('cd-fade-out');
 			}
